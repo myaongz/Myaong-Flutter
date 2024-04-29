47a20c678e5b3d8b99edf02013757f41fe1ca56a
parkyuhyeon <pyh1111802@naver.com>
2024-04-29 20:04:21 +0900

    💄 :: (#21) 링킹페이지 퍼블리싱

---
+++ lib/views/linking/linking_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:myaong/utils/font.dart';
+
+class LinkingPage extends StatelessWidget {
+  const LinkingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: const Row(
+                        children: [
+                          Text(
+                            "링킹",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: pretendard_800,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 32,
+                          ),
+                          Text(
+                            "친구",
+                            style: TextStyle(
+                                fontFamily: pretendard_600,
+                                fontSize: 30,
+                                color: Color(0xffa7a7a7)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/images/home/alert.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 35),
+                Container(
+                  height: 575,
+                  width: 370,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color((0xffD9D9D9)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
